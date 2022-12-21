ff5b8c7f6708f2bf0e4e6ea8a0350e552ca932b0
yts0275 <35211944+yts0275@users.noreply.github.com>
2022-12-21 14:19:58 +0900

    feat : add photo project finish

---
+++ 22-12-21/photo3/photo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.photo_list, name="photo_list"),
+    path('detail/<int:pk>', views.photo_detail, name="photo_detail"),
+    path('photo/new/', views.photo_post, name="photo_post"),
+    path('photo/<int:pk>/edit/', views.photo_edit, name="photo_edit"),
+    path('photo/<int:pk>/delete/', views.photo_delete, name="photo_delete"),
+]
---
+++ 22-12-21/photo3/photo/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Photo
+from .forms import PhotoForm
+
+# Create your views here.
+def photo_list(request):
+    photos = Photo.objects.all()
+    context = {
+        "photos" : photos
+    }
+    # 데이터를 가져와서
+    # context photos
+    return render(request, 'photo/photo_list.html',context)
+
+def photo_detail(request, pk):
+    # photo = Photo.objects.get(pk = pk)
+    # 예시 : http://127.0.0.1:8000/detail/7로 방문하면 (7이 없다는 가정) DoesNotExist error
+    
+    # get_object_or_404(클래스, pk = 옵션)
+    photo = get_object_or_404(Photo, pk = pk)
+    
+    # 데이터를 못가져왔을시, Page not found (404)
+    
+    context = {
+        "photo" : photo
+    }
+    return render(request, 'photo/photo_detail.html', context)
+
+def photo_post(request):
+    # print(request.POST)
+    # POST 요청일때만 
+    
+    if request.method == "POST":
+        # 데이터가 정상적으로 입력되었을때
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.save()
+        # 데이터를 저장
+        return redirect('photo_detail', pk = photo.pk)
+        # 저장후, detail페이지로 "그 입력된 아이디 번호와"
+    else :
+        form = PhotoForm()
+        
+        
+    context = {
+        "form" : form,
+        "title" : "New Photo"
+    }
+    
+    return render(request, 'photo/photo_post.html',context)
+
+def photo_edit(request,pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    # photo_post와 비슷
+    
+    if request.method == "POST":
+        form = PhotoForm(request.POST, instance=photo)
+        if form.is_valid():    
+            form.save()
+            return redirect('photo_detail', pk = photo.pk)
+        # 데이터를 1개 가져올 것, instance 키워드를 사용하면 (수정 = update)
+    else:
+        form = PhotoForm(instance=photo)
+            
+    context = {
+        "form" : form,
+        "title" : "Edit Photo"
+    }
+    
+    return render(request, 'photo/photo_post.html', context)
+
+
+def photo_delete(request, pk):
+    photo = Photo.objects.get(pk = pk)
+    
+    print(request)
+    print(photo)
+    
+    # 삭제요청이 정상적으로 실행되면
+    if request.method == "POST":
+        photo.delete()
+        return redirect('photo_list')
+    
+    #실행에 후, 또는 POST요청이 아니면
+    return redirect('photo_detail', pk = photo.pk)
