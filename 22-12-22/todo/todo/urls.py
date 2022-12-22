4c78ac76c3058aea877376ad67caefbcf051cb34
yts0275 <35211944+yts0275@users.noreply.github.com>
2022-12-22 14:15:04 +0900

    feat : todo project first content

--- 22-12-22/todo/todo/urls.py
+++ 22-12-22/todo/todo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.todo_list, name="todo_list"),
-    path('', views.todo_list, name="todo_list")
+    path('', views.todo_list, name="todo_list"),
+    path('<int:pk>/', views.todo_detail, name="todo_detail"),
+    path('post/', views.todo_post, name= "todo_post")
 ]
--- 22-12-22/todo/todo/views.py
+++ 22-12-22/todo/todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 # Create your views here.
 
 def todo_list(request):
@@ -8,3 +9,26 @@ def todo_list(request):
         'todos' : todos
     }
     return render(request, 'todo_list.html',context)
+
+def todo_detail(request, pk):
+    # todo = Todo.objects.get(id=1)
+    todo = Todo.objects.get(pk=pk)
+    context = {
+        "todo" : todo
+    }
+    return render(request, 'todo_detail.html', context)
+
+def todo_post(request):
+    # print(request.POST)
+    form = TodoForm(request.POST)
+    
+    if form.is_valid():
+        form.save()
+        
+        return redirect("todo_list")
+    
+    context = {
+        "form" : form
+    }
+    
+    return render(request, 'todo_post.html',context)
