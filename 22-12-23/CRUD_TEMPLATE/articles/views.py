95be53cd1688196cd72623cc6e3c5c9d14a36999
yts0275 <35211944+yts0275@users.noreply.github.com>
2022-12-23 10:03:56 +0900

    feat: add logout content

---
+++ 22-12-23/CRUD_TEMPLATE/articles/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@require_POST
+def delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
