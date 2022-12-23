bd5097bf991936a01ae787314ba35eea946104d9
yts0275 <35211944+yts0275@users.noreply.github.com>
2022-12-23 14:27:37 +0900

    feat : add signup content

---
+++ 22-12-23/CRUD_TEMPLATE3/accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods
+# 로그인에 필요
+
+# 로그아웃에 필요
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST
+
+# login_required
+from django.contrib.auth.decorators import login_required
+
+#회원가입 양식
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect("articles:index")
+    
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            #로그인을 시켜라
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or "articles:index")
+
+    else:
+        form = AuthenticationForm()
+    context = {
+    'form' : form
+    }
+    return render(request, 'accounts/login.html',context)
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect("articles:index")
+
+def signup(request):
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else :
+        form = UserCreationForm()
+    context = {
+        "form" : form
+    }
+    return render(request, 'accounts/signup.html', context)
